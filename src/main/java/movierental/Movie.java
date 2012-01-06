b910333ced2f739d7b9cb936945ab7357d1ec85f
Adelina Caramet <adelina.caramet@gmail.com>
2012-01-06 16:31:26 +0200

    Add inheritence & polymorphism

--- src/main/java/movierental/Movie.java
+++ src/main/java/movierental/Movie.java
@@ -2,28 +2,49 @@ package movierental;
 
 public class Movie {
 
-	public static final int CHILDRENS = 2;
-	public static final int NEW_RELEASE = 1;
-	public static final int REGULAR = 0;
-	
-	private String _title;
-	private int _priceCode;
-	
-	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
-	}
-	
-	public int getPriceCode() {
-		return _priceCode;
-	}
+    public static final int CHILDRENS = 2;
+    public static final int NEW_RELEASE = 1;
+    public static final int REGULAR = 0;
 
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
-	}
-	public String getTitle() {
-		return _title;
-	}
-	
-	
+    private String _title;
+    private int _priceCode;
+    Price _price;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        setPriceCode(priceCode);
+    }
+
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
+
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case Movie.REGULAR:
+                _price = new RegularPrice();
+                break;
+            case Movie.NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            case Movie.CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+        }
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    double getCharge(int daysRented) {
+        return _price.getCharge(daysRented);
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
--- src/main/java/movierental/Rental.java
+++ src/main/java/movierental/Rental.java
@@ -5,7 +5,7 @@ package movierental;
  */
 public class Rental {
 
-	private Movie _movie;
+	Movie _movie;
 	private int _daysRented;
 	
 	public Rental(Movie movie, int daysRented) {
@@ -21,31 +21,12 @@ public class Rental {
 		return _movie;
 	}
 
-
     double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            result += 2;
-            if (getDaysRented() > 2)
-                result += (getDaysRented() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            result += getDaysRented() * 3;
-            break;
-        case Movie.CHILDRENS:
-            result += 1.5;
-            if (getDaysRented() > 3)
-                result += (getDaysRented() - 3) * 1.5;
-            break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     int getFrequentRenterPoints() {
-       if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-           return 2;
-       else
-           return 1;
+       return _movie.getFrequentRenterPoints(_daysRented);
    }
+
 }
